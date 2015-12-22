6f4e3f80667bbb8da2811b509b4e864b30c48af7
Junda <junda@just2us.com>
2015-12-23 00:16:24 +0800

    Default assetCollectionSubtypes = nil, so that we don't filter by default

--- QBImagePicker/QBImagePickerController.m
+++ QBImagePicker/QBImagePickerController.m
@@ -28,13 +28,15 @@
     
     if (self) {
         // Set default values
-        self.assetCollectionSubtypes = @[
-                                         @(PHAssetCollectionSubtypeSmartAlbumUserLibrary),
-                                         @(PHAssetCollectionSubtypeAlbumMyPhotoStream),
-                                         @(PHAssetCollectionSubtypeSmartAlbumPanoramas),
-                                         @(PHAssetCollectionSubtypeSmartAlbumVideos),
-                                         @(PHAssetCollectionSubtypeSmartAlbumBursts)
-                                         ];
+        self.assetCollectionSubtypes = nil;     // nil to include all subtypes
+//        Else, set subtypes to filter
+//        self.assetCollectionSubtypes = @[
+//                                         @(PHAssetCollectionSubtypeSmartAlbumUserLibrary),
+//                                         @(PHAssetCollectionSubtypeAlbumMyPhotoStream),
+//                                         @(PHAssetCollectionSubtypeSmartAlbumPanoramas),
+//                                         @(PHAssetCollectionSubtypeSmartAlbumVideos),
+//                                         @(PHAssetCollectionSubtypeSmartAlbumBursts)
+//                                         ];
         self.minimumNumberOfSelection = 1;
         self.numberOfColumnsInPortrait = 4;
         self.numberOfColumnsInLandscape = 7;
