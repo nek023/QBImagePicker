d9f4e8f627c70df6d72addff4b63aa370d112f2b
Mohd Asif <mohd.asif@practo.com>
2017-03-21 10:20:42 +0530

    Fix: collectionview was not scrolling to the end

--- QBImagePicker/QBAssetsViewController.m
+++ QBImagePicker/QBAssetsViewController.m
@@ -107,10 +107,10 @@ static CGSize CGSizeScale(CGSize size, CGFloat scale) {
     if (self.fetchResult.count > 0 && self.isMovingToParentViewController && !self.disableScrollToBottom) {
         // when presenting as a .FormSheet on iPad, the frame is not correct until just after viewWillAppear:
         // dispatching to the main thread waits one run loop until the frame is update and the layout is complete
-        dispatch_async(dispatch_get_main_queue(), ^{
+        [self.collectionView performBatchUpdates:^{} completion: ^(BOOL finished) {
             NSIndexPath *indexPath = [NSIndexPath indexPathForItem:(self.fetchResult.count - 1) inSection:0];
             [self.collectionView scrollToItemAtIndexPath:indexPath atScrollPosition:UICollectionViewScrollPositionTop animated:NO];
-        });
+        }];
     }
 }
 
